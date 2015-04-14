eb6ac92f1f3122b2737a570faea1839121e76a77
sldhana <sldhana@gmail.com>
2015-04-13 22:15:38 -0700

    Removing unique items from a list

---
+++ NonUniques.py
@@ -0,0 +1,19 @@
+"""
+You are given a non-empty list of integers (X).
+For this task, you should return a list consisting of only the
+non-unique elements in this list.
+To do so you will need to remove all unique elements
+(elements which are contained in a given list only once).
+When solving this task, do not change the order of the list.
+Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and
+result will be [1, 3, 1, 3].
+"""
+
+def remove_non_uniques(list):
+    return [i for i in list if list.count(i) > 1]
+
+
+print remove_non_uniques([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+print remove_non_uniques([1, 2, 3, 4, 5]) == []
+print remove_non_uniques([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+print remove_non_uniques([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
