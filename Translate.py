46304265e23f16876f7e0ce0127b839f50faf5f8
sldhana <sldhana@ymail.com>
2015-04-16 22:08:14 -0700

    Commented code

--- Translate.py
+++ Translate.py
@@ -1,4 +1,8 @@
 def translate(number):
+    """
+    Use the number map below for numbers that follow a certain pattern.  For example,
+    12 will uniquely be called twelve and cannot be formed dynamically.
+    """
     number_map = {
         1: "one",
         2: "two",
@@ -30,21 +34,25 @@ def translate(number):
     }
 
 
-    """
-    number is 4
-    """
+    #this will hold the string representation of the number
     translated = ""
 
+    """
+    the first check is to make sure the number is greater than 0.  We need to do this
+    to ensure the loop ends
+    """
     while number > 0:
+        #first check if the number is the map, if so, return value and decrement it
         if number in number_map:
             translated += " " + number_map[number]
             number -= number
-        if number <= 0:
             break
-        if number <= 10:
-            translated += " " + number_map[number]
-            number -= number
-        elif number <= 99:
+        """
+        the logic below are for numbers that aren't in the list above.  For example,
+        if the number is 98, it will first get "90" from the number and map it to the
+        list above.  Then, it takes 98 - 90 and uses the balance to form the rest of the string
+        """
+        if number <= 99:
             translated += " " + number_map[(number/10) * 10]
             number = number - (number/10) * 10
         elif number <= 999:
@@ -53,6 +61,11 @@ def translate(number):
         elif number >= 1000:
             translated += " " + number_map[(number/1000)] + " thousand"
             number = number - (number/1000 * 1000)
+
+    """
+    since we added a space in the string, it returns the string chopping off the first character.
+    There is probably a better way to do this, but I didn't want to spend too much time on it
+    """
 
     print translated[1:]
     return translated[1:]
@@ -63,3 +76,4 @@ translate(905)
 translate(99)
 translate(11)
 translate(5)
+translate(10)
